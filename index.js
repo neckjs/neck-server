158df41b0d601b94115aee7f50d35ee84a20eed2
José Moreira <jmoreira@microdual.com>
2014-09-14 23:10:30 +0100

    exports

--- index.js
+++ index.js
@@ -2,7 +2,10 @@
  * @class Neck.Server
  * returns Server
  */
+var Server = require( './lib/server' );
 
-require( './lib/client.js' );
+// Give access to Server Classes
+Server.Client = require( './lib/client.js' );
+Server.Task = require( './lib/task.js' );
 
-module.exports = require( './lib/server' );
+module.exports = Server;
